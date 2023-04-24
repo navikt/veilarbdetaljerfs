d0f1549220bcb033da611bf9690f5f01368a4687
Sondre Larsen Ovrid <sondre.larsen.ovrid@nav.no>
2023-04-24 14:45:28 +0200

    Installer og ta i bruk NAVSPA

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import NAVSPA from "@navikt/navspa"
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+
+const erLokal = true;
+
+if (erLokal) {
+    ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+        <React.StrictMode>
+            <App/>
+        </React.StrictMode>,
+    )
+} else {
+    NAVSPA.eksporter('veilarbdetaljerfs', App);
+}
