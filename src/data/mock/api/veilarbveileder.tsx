dc5be95d7d07e4ac61d49e31ebfcc68bf7898024
Kristine Schuessler <kristine.lynne.schuessler@nav.no>
2023-06-23 14:36:46 +0200

    Koble opp api og mock api

---
+++ src/data/mock/api/veilarbveileder.tsx
@@ -0,0 +1,16 @@
+import { rest } from 'msw';
+import { RequestHandler } from 'msw';
+import { VeilederData } from '../../datatyper/veileder';
+
+const veileder: VeilederData = {
+	etternavn: 'Veiledersen',
+	fornavn: 'TJ',
+	ident: 'Z123456',
+	navn: 'TJ Veiledersen'
+};
+
+export const veilarbveilederHandlers: RequestHandler[] = [
+	rest.get('/veilarbveileder/api/veileder/:veilederId', (req, res, ctx) => {
+		return res(ctx.delay(500), ctx.json(veileder));
+	})
+];
