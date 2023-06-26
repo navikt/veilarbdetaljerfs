d8551169f72248caa5e50f418570dcc67df3b12f
Stian Alexander Solli <stian.alexander.solli@nav.no>
2023-06-26 15:26:29 +0200

    Flyttet options som brukes i API-kall (method, headers etc.) til egen fil som gjenbrukes. Endret fra default export til named export.

--- src/data/api/hentVeileder.tsx
+++ src/data/api/hentVeileder.tsx
@@ -1,15 +1,9 @@
-const hentVeileder = async (veilederId: string): Promise<any> => {
+import { GEToptions } from './datatyper/apiGetOptions';
+
+export const hentVeileder = async (veilederId: string): Promise<any> => {
     const url = `/veilarbveileder/api/veileder/${veilederId}`;
-    const options = {
-        method: 'GET',
-        headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json;charset=UTF-8'
-        }
-    };
-    const response = await fetch(url, options);
+
+    const response = await fetch(url, GEToptions);
     const data = await response.json();
     return data;
 };
-
-export default hentVeileder;
