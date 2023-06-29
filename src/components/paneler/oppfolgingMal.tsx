e4e1ce106dd3e4c5d01cb04b9aa2ce5fd740c9c7
kristinelynneschuessler <136683349+kristinelynneschuessler@users.noreply.github.com>
2023-06-29 15:02:00 +0200

    refaktorer API kall (#17)
    * refaktorer API kall
    
    * fjern credentials

--- src/components/paneler/oppfolgingMal.tsx
+++ src/components/paneler/oppfolgingMal.tsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from 'react';
-import hentOppfolging from '../../data/api/hentOppfolging';
+import { hentOppfolgingsstatus } from '../../data/api/fetch';
 import { OppfolgingsstatusData } from '../../data/api/datatyper/oppfolgingsstatus';
 
 const OppfolgingMal = () => {
     const [oppfolging, setOppfolging] = useState<OppfolgingsstatusData | null>(null);
 
     useEffect(() => {
-        hentOppfolging('10108000398').then((data) => {
+        hentOppfolgingsstatus('10108000398').then((data) => {
             setOppfolging(data);
         });
     }, []);
