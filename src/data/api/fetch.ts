2149e8e06c01ee7c6af1183ed4d8e5c7dbc2e419
stiansolliNAV <136702738+stiansolliNAV@users.noreply.github.com>
2023-07-05 14:28:42 +0200

    Lagt til håndtering for http status koder som ikke skal gi error (#25)
    * La til sourcemap i vite config
    
    * Sjekk for tom data ved api-kall
    
    * Kjør prettier på src
    
    * Trekk ut respons-handling til separat funksjon
    
    * Setter default loading state (lasterData) til true
    
    ---------
    
    Co-authored-by: Sondre Larsen Ovrid <sondre.larsen.ovrid@nav.no>

--- src/data/api/fetch.ts
+++ src/data/api/fetch.ts
@@ -7,75 +7,61 @@ import { StringOrNothing } from '../../utils/felles-typer';
 import { VeilederData } from './datatyper/veileder';
 import { YtelseData } from './datatyper/ytelse';
 
+const handterRespons = async (respons: Response) => {
+    if (respons.status >= 400) {
+        throw new Error(respons.statusText);
+    }
 
-export const hentOppfolgingsstatus = async (fnr: string): Promise<OppfolgingsstatusData> => {
+    if (respons.status === 204) {
+        return null;
+    }
+
+    try {
+        return await respons.json();
+    } catch (error) {
+        console.log('Error ved parsing:', error);
+        return null;
+    }
+};
+
+export const hentOppfolgingsstatus = async (fnr: string): Promise<OppfolgingsstatusData | null> => {
     const url = `/veilarboppfolging/api/person/${fnr}/oppfolgingsstatus`;
-    const response = await fetch(url, GEToptions);
+    const respons = await fetch(url, GEToptions);
 
-    if (!response.ok) {
-        throw new Error(response.statusText)
-      }
-
-    const data = await response.json();
-    return data;
+    return handterRespons(respons);
 };
 
-export const hentPersonalia = async (fnr: string): Promise<PersonaliaV2Info> => {
+export const hentPersonalia = async (fnr: string): Promise<PersonaliaV2Info | null> => {
     const url = `/veilarbperson/api/v2/person?fnr=${fnr}`;
-    const response = await fetch(url, GEToptions);
+    const respons = await fetch(url, GEToptions);
 
-    if (!response.ok) {
-        throw new Error(response.statusText)
-      }
-
-    const data = await response.json();
-    return data;
+    return handterRespons(respons);
 };
 
-export const hentRegistrering = async (fnr: string): Promise<RegistreringsData> => {
+export const hentRegistrering = async (fnr: string): Promise<RegistreringsData | null> => {
     const url = `/veilarbperson/api/person/registrering?fnr=${fnr}`;
-    const response = await fetch(url, GEToptions);
+    const respons = await fetch(url, GEToptions);
 
-    if (!response.ok) {
-        throw new Error(response.statusText)
-      }
-    
-    const data = await response.json();
-    return data;
+    return handterRespons(respons);
 };
 
-export const hentTolk = async (fnr: string): Promise<TilrettelagtKommunikasjonData> => {
+export const hentTolk = async (fnr: string): Promise<TilrettelagtKommunikasjonData | null> => {
     const url = `/veilarbperson/api/v2/person/tolk?fnr=${fnr}`;
-    const response = await fetch(url, GEToptions);
+    const respons = await fetch(url, GEToptions);
 
-    if (!response.ok) {
-        throw new Error(response.statusText)
-      }
-    
-    const data = await response.json();
-    return data;
+    return handterRespons(respons);
 };
 
-export const hentVeileder = async (veilederId: StringOrNothing): Promise<VeilederData> => {
+export const hentVeileder = async (veilederId: StringOrNothing): Promise<VeilederData | null> => {
     const url = `/veilarbveileder/api/veileder/${veilederId}`;
-    const response = await fetch(url, GEToptions);
+    const respons = await fetch(url, GEToptions);
 
-    if (!response.ok) {
-        throw new Error(response.statusText)
-      }
-    
-    const data = await response.json();
-    return data;
+    return handterRespons(respons);
 };
 
-export const hentYtelser = async (fnr: string): Promise<YtelseData> => {
+export const hentYtelser = async (fnr: string): Promise<YtelseData | null> => {
     const url = `/veilarboppfolging/api/person/${fnr}/ytelser`;
-    const response = await fetch(url, GEToptions);
+    const respons = await fetch(url, GEToptions);
 
-    if (!response.ok) {
-        throw new Error(response.statusText)
-      }
-    
-    const data = await response.json();
-    return data;
+    return handterRespons(respons);
 };
