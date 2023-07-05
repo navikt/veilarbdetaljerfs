e7525695dd4660a9cbb216f9f1bc6205c7d50aa1
SanneEdlandsommerstudent <136682983+SanneEdlandsommerstudent@users.noreply.github.com>
2023-07-05 11:34:48 +0200

    Pilot boks (#22)
    * Lagt til pilot info boks
    
    * Endret link til forms
    
    * Implementer mapping (#20)
    
    * Oppdater datatyper, legg til default verdi, formattering og feilhåndtering
    
    * FIX: uhåndtert undefined verdi
    
    * FIX: En string var skrevet feil
    
    * Filtrerte på barn under 21
    
    * FIX: Kommentarer på dobble variabler og fiks i formatteringsdata
    
    * FIX: Endret Overblikk til Nøkkelinfo
    
    * Fjernet kommentert kode
    
    * Fikset opp i kommentarer
    
    ---------
    
    Co-authored-by: Sanne Edland <sanne.edland@nav.no>
    
    * Fikset css
    
    * La til pilotboks
    
    * Api error catching and loading state (#21)
    
    * Fiks parsing feil
    
    Versjon 1.2.2 av msw har en bug som gjør at det blir spyttet ut en del feil ifm. at msw forsøker å lese samme
    respons-stream flere ganger. Nedgraderer derfor til 1.2.1 som ikke gir samme feil.
    
    Ref: https://github.com/mswjs/msw/issues/1640
    
    * Kjør npm audit fix
    
    * Håndter loading og error state
    
    * Laget felleskomponenter for error-melding og loader til å bruke med api-kall
    
    * Refaktorert datahenting i overblikk, håndterer nå errors. Lagt på loader og errormelding for UX ved datahenting.
    
    * API-kallene vil throwe error når HTTP status koden er alt annet enn 2xx
    
    * Endret navn på overblikk komponenten
    
    ---------
    
    Co-authored-by: Sondre Larsen Ovrid <sondre.larsen.ovrid@nav.no>
    
    * Merge
    
    ---------
    
    Co-authored-by: kristinelynneschuessler <136683349+kristinelynneschuessler@users.noreply.github.com>
    Co-authored-by: stiansolliNAV <136702738+stiansolliNAV@users.noreply.github.com>
    Co-authored-by: Sondre Larsen Ovrid <sondre.larsen.ovrid@nav.no>

---
+++ src/components/pilotAlert.tsx
@@ -0,0 +1,17 @@
+import { Alert, Heading } from "@navikt/ds-react";
+import {
+    ExternalLinkIcon
+} from '@navikt/aksel-icons';
+
+export const PilotAlert = () => {
+    return (
+        <Alert variant="warning" className="PilotAlert">
+            <Heading spacing size="small" level="3">
+                Testside for Overblikk
+            </Heading>
+            <p>På denne siden vil det dukke opp nytt innhold fortløpende. Vi setter stor pris på dine tilbakemeldinger, som du kan gi når som helst og så mange ganger du vil via skjemaet under. Takk for at du deltar!
+            </p>
+            <a href="https://forms.office.com/e/xkbbUv07y0">Gi tilbakemeldinger i Forms <ExternalLinkIcon title="a11y-title" /></a>
+        </Alert>
+    );
+};
