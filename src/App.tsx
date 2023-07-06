5a91ac230a7b663467f7c8fb4a35d9b4ffcd43ac
Sanne Edland <sanne.edland@nav.no>
2023-07-06 10:43:32 +0200

    run prettier

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ const App = (props: AppProps) => {
         <main className="app veilarbdetaljerfs">
             <StoreProvider fnr={props.fnr}>
                 <Nokkelinfo />
-                <div className='main_grid'>
+                <div className="main_grid">
                     <InfoBoks />
                 </div>
             </StoreProvider>
