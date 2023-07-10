bef7694ae6e054349efbf96a845a249449dc0e54
Stian Alexander Solli <stian.alexander.solli@nav.no>
2023-07-10 16:29:03 +0200

    Fjernet visning av CV

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import Nokkelinfo from './components/nokkelinfo';
-import CvInnhold from './components/cv/cv-innhold';
 import StoreProvider from './stores/store-provider';
 
 export interface AppProps {
@@ -12,9 +11,8 @@ const App = (props: AppProps) => {
         <main className="app veilarbdetaljerfs">
             <StoreProvider fnr={props.fnr}>
                 <Nokkelinfo />
-                <div className="main_grid">
-                    <CvInnhold />
-                </div>
+
+                <div className="main_grid"></div>
             </StoreProvider>
         </main>
     );
