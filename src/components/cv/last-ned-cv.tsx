1d7aff0d5109006913972e61517f016c5a953474
Stian Alexander Solli <stian.alexander.solli@nav.no>
2023-07-12 09:16:40 +0200

    Refaktorert og endret på skrivefeil

--- src/components/cv/last-ned-cv.tsx
+++ src/components/cv/last-ned-cv.tsx
@@ -1,14 +1,16 @@
 import { logMetrikk } from '../../utils/logger';
 import { DownloadIcon } from '@navikt/aksel-icons';
 import { Link } from '@navikt/ds-react';
+import { byggPamUrl } from '../../utils';
 
-export function LastNedCV(props: { erManuell: boolean | undefined; lastNedCvLenke: string }) {
+export function LastNedCV(props: { erManuell: boolean | undefined; fnr: string }) {
     const handleOnLastNedLenkeClicked = () => {
-        logMetrikk('veilarbmaofs.metrikker.last-ned-cv', { erManuell: props.erManuell });
+        logMetrikk('veilarbdetaljerfs.metrikker.last-ned-cv', { erManuell: props.erManuell });
     };
+    const lastNedCvUrl = byggPamUrl(props.fnr, '/cv/pdf');
 
     return (
-        <Link onClick={handleOnLastNedLenkeClicked} href={props.lastNedCvLenke} target="_blank">
+        <Link onClick={handleOnLastNedLenkeClicked} href={lastNedCvUrl} target="_blank">
             <DownloadIcon />
             Last ned CV
         </Link>
--- src/data/api/datatyper/apiOptions.ts
+++ src/data/api/datatyper/apiOptions.ts
@@ -9,14 +9,10 @@ export const GEToptions = {
     }
 };
 
-export const createPOSToptions = (event: FrontendEvent) => {
-    const POSToptions = {
-        method: 'POST',
-        body: JSON.stringify(event),
-        headers: {
-            'Nav-Consumer-Id': 'veilarbdetaljerfs'
-        }
-    };
-
-    return POSToptions;
-};
+export const createPOSToptions = (event: FrontendEvent) => ({
+    method: 'POST',
+    body: JSON.stringify(event),
+    headers: {
+        'Nav-Consumer-Id': 'veilarbdetaljerfs'
+    }
+});
