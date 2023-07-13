2e6691d71ca96b75ae276c7e5dd45a0ae1edce8f
Kristine Schuessler <kristine.lynne.schuessler@nav.no>
2023-07-13 14:42:15 +0200

    endre css styling ref code review og fjernet komponentene i app

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,7 @@ import CvInnhold from './components/cv/cv-innhold';
 import Nokkelinfo from './components/nokkelinfo';
 import Oppfolgning from './components/oppfolgning';
 import PersonaliaBoks from './components/personalia/personalia-boks';
-import { Registrering } from './components/registreringsInfo';
-import { Ytelser } from './components/ytelserinfo';
+
 import StoreProvider from './stores/store-provider';
 
 export interface AppProps {
@@ -16,13 +15,7 @@ const App = (props: AppProps) => {
         <main className="app veilarbdetaljerfs">
             <StoreProvider fnr={props.fnr}>
                 <Nokkelinfo />
-                <div className="main_grid">
-                    <CvInnhold />
-                    <Ytelser />
-                    <Oppfolgning />
-                    <PersonaliaBoks />
-                    <Registrering />
-                </div>
+                <div className="main_grid"></div>
             </StoreProvider>
         </main>
     );
