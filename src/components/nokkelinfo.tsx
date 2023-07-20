fb76db2dd37d4c1791e791803fe6ad8c28258d99
stiansolliNAV <136702738+stiansolliNAV@users.noreply.github.com>
2023-07-20 13:44:44 +0200

    Api endre til swr (#51)
    * Endret til datahenting ved bruk av SWR-hooks
    
    * Slettet gammel fetch-fil
    
    * Endret filnavn og oppdatert imports
    
    * Lagt til håndtering for HTTP 204 og 404, siden fetch thrower error på 204 og 404
    
    * Pakket ut data, loading og error til egne variabler for alle datapunktene
    
    * Vil conditionally fetche veileder dersom veilederID er tilgjengelig

--- src/components/nokkelinfo.tsx
+++ src/components/nokkelinfo.tsx
@@ -1,107 +1,92 @@
 import { Heading, Panel } from '@navikt/ds-react';
 import { Laster, Errormelding } from './felles/minikomponenter';
 import './nokkelinfo.css';
-import { VeilederData } from '../data/api/datatyper/veileder';
 import { useAppStore } from '../stores/app-store';
 import {
-    hentOppfolgingsstatus,
-    hentPersonalia,
-    hentRegistrering,
-    hentTolk,
-    hentVeileder,
-    hentYtelser,
-    hentCvOgJobbonsker
+    useOppfolgingsstatus,
+    usePersonalia,
+    useRegistrering,
+    useTolk,
+    useVeileder,
+    useYtelser,
+    useCvOgJobbonsker
 } from '../data/api/fetch';
-import { OppfolgingsstatusData, ArenaHovedmalKode } from '../data/api/datatyper/oppfolgingsstatus';
-import { useEffect, useState } from 'react';
-import { PersonaliaV2Info, PersonsBarn } from '../data/api/datatyper/personalia';
-import { RegistreringsData } from '../data/api/datatyper/registreringsData';
+import { ArenaHovedmalKode } from '../data/api/datatyper/oppfolgingsstatus';
+import { PersonsBarn } from '../data/api/datatyper/personalia';
 import { TilrettelagtKommunikasjonData } from '../data/api/datatyper/tilrettelagtKommunikasjon';
-import { YtelseData } from '../data/api/datatyper/ytelse';
 import { OrNothing, StringOrNothing } from '../utils/felles-typer';
 import { EnkeltInformasjon } from './felles/enkeltInfo';
 import { getVedtakForVisning, hentTolkTekst, hentVeilederTekst, mapHovedmalTilTekst } from '../utils/text-mapper';
 import { Hovedmal } from '../data/api/datatyper/siste14aVedtak';
 import { formaterDato, formaterTelefonnummer } from '../utils/formater';
-import { ArenaPerson } from '../data/api/datatyper/arenaperson';
 import { kalkulerAlder } from '../utils/date-utils';
 import { EnkeltInformasjonMedCopy } from './felles/enkeltInfoMedCopy';
 
 const Nokkelinfo = () => {
     const { fnr } = useAppStore();
 
-    const [lasterData, setLasterData] = useState<boolean>(true);
-    const [harFeil, setHarFeil] = useState<boolean>(false);
+    const {
+        data: oppfolgingsstatusData,
+        error: oppfolgingsstatusError,
+        isLoading: oppfolgingsstatusLoading
+    } = useOppfolgingsstatus(fnr);
+    const { data: personData, error: personError, isLoading: personLoading } = usePersonalia(fnr);
+    const { data: registreringData, error: registreringError, isLoading: registreringLoading } = useRegistrering(fnr);
+    const { data: tolkData, error: tolkError, isLoading: tolkLoading } = useTolk(fnr);
+    const { data: ytelserData, error: ytelserError, isLoading: ytelserLoading } = useYtelser(fnr);
+    const {
+        data: cvOgJobbonskerData,
+        error: cvOgJobbonskerError,
+        isLoading: cvOgJobbonskerLoading
+    } = useCvOgJobbonsker(fnr);
 
-    const [veileder, setVeileder] = useState<VeilederData | null>(null);
-    const [oppfolgingsstatus, setOppfolgingsstatus] = useState<OppfolgingsstatusData | null>(null);
-    const [person, setPerson] = useState<PersonaliaV2Info | null>(null);
-    const [registrering, setRegistrering] = useState<RegistreringsData | null>(null);
-    const [tolk, setTolk] = useState<TilrettelagtKommunikasjonData | null>(null);
-    const [ytelser, setYtelser] = useState<YtelseData | null>(null);
-    const [cvOgJobbonsker, setCvOgJobbonsker] = useState<ArenaPerson | null>(null);
+    const {
+        data: veilederData,
+        error: veilederError,
+        isLoading: veilederLoading
+    } = useVeileder(oppfolgingsstatusData?.veilederId);
 
-    useEffect(() => {
-        const hentOverblikkData = async () => {
-            try {
-                setLasterData(true);
-                const [_oppfolgingsstatus, _personalia, _registrering, _tolk, _ytelser, _cvOgJobbonsker] =
-                    await Promise.all([
-                        hentOppfolgingsstatus(fnr),
-                        hentPersonalia(fnr),
-                        hentRegistrering(fnr),
-                        hentTolk(fnr),
-                        hentYtelser(fnr),
-                        hentCvOgJobbonsker(fnr)
-                    ]);
-
-                if (!!_oppfolgingsstatus && !!_oppfolgingsstatus?.veilederId) {
-                    const _veileder = await hentVeileder(_oppfolgingsstatus.veilederId);
-                    setVeileder(_veileder);
-                }
-
-                setOppfolgingsstatus(_oppfolgingsstatus);
-                setPerson(_personalia);
-                setRegistrering(_registrering);
-                setTolk(_tolk);
-                setYtelser(_ytelser);
-                setCvOgJobbonsker(_cvOgJobbonsker);
-            } catch (err) {
-                setHarFeil(true);
-            } finally {
-                setLasterData(false);
-            }
-        };
-
-        hentOverblikkData();
-    }, [fnr]);
-
-    const telefon: StringOrNothing = person?.telefon?.find((entry) => entry.prioritet === '1')?.telefonNr;
-    const taletolk: OrNothing<TilrettelagtKommunikasjonData> = tolk;
-    const onsketYrkeTitles: string[] = cvOgJobbonsker?.jobbprofil?.onsketYrke.map((yrke) => yrke.tittel) || [];
-    const sivilstatus: StringOrNothing = person?.sivilstandliste?.[0]?.sivilstand;
-    const hovedmaal: OrNothing<Hovedmal | ArenaHovedmalKode> = oppfolgingsstatus?.hovedmaalkode;
-    const registrertAv: StringOrNothing = registrering?.registrering?.manueltRegistrertAv?.enhet?.navn;
-    const datoRegistrert: StringOrNothing = registrering?.registrering?.opprettetDato;
-    const MAX_ALDER_BARN = 21;
-    const barnUnder21: PersonsBarn[] =
-        (person?.barn &&
-            person.barn.filter((enkeltBarn) => kalkulerAlder(new Date(enkeltBarn.fodselsdato)) < MAX_ALDER_BARN)) ||
-        [];
-
-    const barnNavn: string = barnUnder21
-        .map((barn) => `${barn.fornavn} (${kalkulerAlder(new Date(barn.fodselsdato))})`)
-        .join(', ');
-
-    if (lasterData) {
+    if (
+        oppfolgingsstatusLoading ||
+        personLoading ||
+        registreringLoading ||
+        tolkLoading ||
+        ytelserLoading ||
+        cvOgJobbonskerLoading ||
+        veilederLoading
+    ) {
         return (
             <Panel border className="nokkelinfo_panel" tabIndex={1}>
                 <Laster />
             </Panel>
         );
     }
-
-    if (harFeil) {
+    if (
+        oppfolgingsstatusError?.status === 204 ||
+        oppfolgingsstatusError?.status === 404 ||
+        personError?.status === 204 ||
+        personError?.status === 404 ||
+        registreringError?.status === 204 ||
+        registreringError?.status === 404 ||
+        tolkError?.status === 204 ||
+        tolkError?.status === 404 ||
+        ytelserError?.status === 204 ||
+        ytelserError?.status === 404 ||
+        cvOgJobbonskerError?.status === 204 ||
+        cvOgJobbonskerError?.status === 404 ||
+        veilederError?.status === 204 ||
+        veilederError?.status === 404
+    ) {
+        // Pass fordi 204 og 404 thrower error, vil ikke vise feilmelding, men lar komponentene håndtere hvis det ikke er noe data
+    } else if (
+        oppfolgingsstatusError ||
+        personError ||
+        registreringError ||
+        tolkError ||
+        ytelserError ||
+        cvOgJobbonskerError ||
+        veilederError
+    ) {
         return (
             <Panel border className="nokkelinfo_panel" tabIndex={1}>
                 <Heading spacing level="2" size="medium">
@@ -111,6 +96,25 @@ const Nokkelinfo = () => {
             </Panel>
         );
     }
+
+    const telefon: StringOrNothing = personData?.telefon?.find((entry) => entry.prioritet === '1')?.telefonNr;
+    const taletolk: OrNothing<TilrettelagtKommunikasjonData> = tolkData;
+    const onsketYrkeTitles: string[] = cvOgJobbonskerData?.jobbprofil?.onsketYrke.map((yrke) => yrke.tittel) || [];
+    const sivilstatus: StringOrNothing = personData?.sivilstandliste?.[0]?.sivilstand;
+    const hovedmaal: OrNothing<Hovedmal | ArenaHovedmalKode> = oppfolgingsstatusData?.hovedmaalkode;
+    const registrertAv: StringOrNothing = registreringData?.registrering?.manueltRegistrertAv?.enhet?.navn;
+    const datoRegistrert: StringOrNothing = registreringData?.registrering?.opprettetDato;
+    const MAX_ALDER_BARN = 21;
+    const barnUnder21: PersonsBarn[] =
+        (personData?.barn &&
+            personData?.barn.filter(
+                (enkeltBarn) => kalkulerAlder(new Date(enkeltBarn.fodselsdato)) < MAX_ALDER_BARN
+            )) ||
+        [];
+
+    const barnNavn: string = barnUnder21
+        .map((barn) => `${barn.fornavn} (${kalkulerAlder(new Date(barn.fodselsdato))})`)
+        .join(', ');
 
     return (
         <Panel border className="nokkelinfo_panel" tabIndex={1}>
@@ -122,12 +126,12 @@ const Nokkelinfo = () => {
                 <EnkeltInformasjon header="Barn under 21 år" value={barnNavn} />
                 <EnkeltInformasjon header="Hovedmål" value={mapHovedmalTilTekst(hovedmaal)} />
                 <EnkeltInformasjon header="Registrert dato" value={formaterDato(datoRegistrert)} />
-                <EnkeltInformasjon header="Veileder" value={hentVeilederTekst(veileder)} />
+                <EnkeltInformasjon header="Veileder" value={hentVeilederTekst(veilederData)} />
                 <EnkeltInformasjon header="Tilrettelagt kommunikasjon" value={hentTolkTekst(taletolk)} />
                 <EnkeltInformasjon header="Sivilstand" value={sivilstatus} />
                 <EnkeltInformasjon header="Jobbønsker" value={onsketYrkeTitles.join(', ')} />
                 <EnkeltInformasjon header="Registrert av" value={registrertAv} />
-                <EnkeltInformasjon header="Aktive ytelse(r)" value={getVedtakForVisning(ytelser?.vedtaksliste)} />
+                <EnkeltInformasjon header="Aktive ytelse(r)" value={getVedtakForVisning(ytelserData?.vedtaksliste)} />
             </span>
         </Panel>
     );
