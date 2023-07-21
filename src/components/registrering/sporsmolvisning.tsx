eaaaa9979ec8e8f6461519a55ee7215ce6cf2da1
Stian Alexander Solli <stian.alexander.solli@nav.no>
2023-07-21 09:46:41 +0200

    Endret fra function til arrow function

--- src/components/registrering/sporsmolvisning.tsx
+++ src/components/registrering/sporsmolvisning.tsx
@@ -1,7 +1,7 @@
 import { Registrering, Sporsmal } from '../../data/api/datatyper/registreringsData';
 import { EnkeltInformasjon } from '../felles/enkeltInfo';
 
-export function SporsmalsListe(props: { registrering?: Registrering }) {
+export const SporsmalsListe = (props: { registrering?: Registrering }) => {
     if (!props.registrering || !props.registrering.teksterForBesvarelse) {
         return null;
     }
@@ -9,8 +9,8 @@ export function SporsmalsListe(props: { registrering?: Registrering }) {
     const sporsmaal = props.registrering.teksterForBesvarelse;
 
     return <span className="info_container">{sporsmaal.map(sporsmalvisning)}</span>;
-}
+};
 
-function sporsmalvisning(sporsmal: Sporsmal) {
+const sporsmalvisning = (sporsmal: Sporsmal) => {
     return <EnkeltInformasjon header={sporsmal.sporsmal} value={sporsmal.svar} key={sporsmal.sporsmalId} />;
-}
+};
