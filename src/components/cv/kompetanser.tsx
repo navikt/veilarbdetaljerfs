fb00074a218beb0eeac4f174399d761897c8969c
Sanne Edland <sanne.edland@nav.no>
2023-07-25 13:54:23 +0200

    Brukt komponentbiblioteket istedenfor vanlig html

--- src/components/cv/kompetanser.tsx
+++ src/components/cv/kompetanser.tsx
@@ -1,22 +1,23 @@
-import { BodyShort } from '@navikt/ds-react';
+import { BodyShort, List } from '@navikt/ds-react';
 import { Jobbprofil } from '../../data/api/datatyper/arenaperson';
 import EMDASH from '../../utils/emdash';
 import Informasjonsbolk from '../felles/informasjonsbolk';
 import { FolderFileIcon } from '@navikt/aksel-icons';
+import ListItem from '@navikt/ds-react/esm/list/ListItem';
 
 const Kompetanser = ({ kompetanse }: Pick<Jobbprofil, 'kompetanse'>) => {
     const kompetanser =
         kompetanse && kompetanse.length > 0
             ? kompetanse?.map((kompetansen) => (
-                  <li>
+                  <ListItem>
                       <BodyShort size="small">{kompetansen.tittel}</BodyShort>
-                  </li>
+                  </ListItem>
               ))
             : EMDASH;
 
     return (
         <Informasjonsbolk header="Kompetanser" icon={<FolderFileIcon />} headerTypo="ingress">
-            <ul> {kompetanser}</ul>
+            <List>{kompetanser}</List>
         </Informasjonsbolk>
     );
 };
