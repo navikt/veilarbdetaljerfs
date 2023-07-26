538b10845d4b354460742db55aace7f631e8a0b6
Sanne Edland <136682983+SanneEdlandsommerstudent@users.noreply.github.com>
2023-07-26 10:07:42 +0200

    Brukt komponentbiblioteket istedenfor vanlig html (#65)
    * Brukt komponentbiblioteket istedenfor vanlig html
    
    * Update src/components/cv/kompetanser.tsx
    
    Co-authored-by: Sondre Ovrid <111113539+slovrid@users.noreply.github.com>
    
    * prettier run
    
    ---------
    
    Co-authored-by: Sondre Ovrid <111113539+slovrid@users.noreply.github.com>

--- src/components/cv/kompetanser.tsx
+++ src/components/cv/kompetanser.tsx
@@ -1,22 +1,23 @@
-import { BodyShort } from '@navikt/ds-react';
+import { BodyShort, List } from '@navikt/ds-react';
 import { Jobbprofil } from '../../data/api/datatyper/arenaperson';
 import EMDASH from '../../utils/emdash';
 import Informasjonsbolk from '../felles/informasjonsbolk';
 import { FolderFileIcon } from '@navikt/aksel-icons';
+import ListItem from '@navikt/ds-react/esm/list/ListItem';
 
 const Kompetanser = ({ kompetanse }: Pick<Jobbprofil, 'kompetanse'>) => {
     const kompetanser =
         kompetanse && kompetanse.length > 0
             ? kompetanse?.map((kompetansen) => (
-                  <li>
-                      <BodyShort size="small">{kompetansen.tittel}</BodyShort>
-                  </li>
+                  <ListItem>
+                      <BodyShort size="small">{kompetansen.tittel}</BodyShort>s{' '}
+                  </ListItem>
               ))
             : EMDASH;
 
     return (
         <Informasjonsbolk header="Kompetanser" icon={<FolderFileIcon />} headerTypo="ingress">
-            <ul> {kompetanser}</ul>
+            <List as="ul">{kompetanser}</List>
         </Informasjonsbolk>
     );
 };
