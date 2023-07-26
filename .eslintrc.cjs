647eefbcc5915b28e7c4e3cdf776ccb424c9946f
Sondre Larsen Ovrid <sondre.larsen.ovrid@nav.no>
2023-07-26 10:10:02 +0200

    Oppdater eslint-config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,10 +1,16 @@
 module.exports = {
     env: { browser: true, es2020: true },
-    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended'],
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:jsx-a11y/recommended'
+    ],
     parser: '@typescript-eslint/parser',
     parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-    plugins: ['react-refresh'],
+    plugins: ['@typescript-eslint', 'react-refresh'],
     rules: {
         'react-refresh/only-export-components': 'warn'
-    }
+    },
+    root: true
 };
--- src/App.tsx
+++ src/App.tsx
@@ -55,7 +55,7 @@ const App = (props: AppProps) => {
                         <Heading spacing size="small" level="3">
                             Testside for Overblikk
                         </Heading>
-                        <BodyShort>
+                        <BodyShort tabIndex={2}>
                             På denne siden vil det dukke opp nytt innhold fortløpende. Vi setter stor pris på dine
                             tilbakemeldinger, som du kan gi når som helst og så mange ganger du vil via skjemaet under.
                             Takk for at du deltar!
