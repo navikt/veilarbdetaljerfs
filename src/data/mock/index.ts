10df2a34b7bb3d5e3b83b3c1116ac7bade04524c
Sondre Ovrid <111113539+slovrid@users.noreply.github.com>
2023-08-18 12:26:13 +0200

    Fikse oppsett for deploy til GitHub-pages (#81)

--- src/data/mock/index.ts
+++ src/data/mock/index.ts
@@ -3,7 +3,7 @@ import { allHandlers } from './api';
 
 // This configures a Service Worker with the given request handlers.
 export const worker = setupWorker(...allHandlers)
-    .start({ serviceWorker: { url: '/mockServiceWorker.js' } })
+    .start({ serviceWorker: { url: `${import.meta.env.BASE_URL}mockServiceWorker.js` } })
     .catch((e) => {
         console.error('Unable to setup mocked API endpoints', e);
     });
