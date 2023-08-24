b040f91b5e79ea4518523b59d013812303222efe
Oda Dahlen <oda.dahlen@nav.no>
2023-08-24 14:12:52 +0200

    Fjernet boksen

--- src/App.tsx
+++ src/App.tsx
@@ -71,20 +71,6 @@ const App = (props: AppProps) => {
             >
                 <div className="overblikk">
                     <StoreProvider fnr={props.fnr}>
-                        <Alert variant="warning" className="pilot_alert">
-                            <Heading spacing size="small" level="3">
-                                Testside for Overblikk
-                            </Heading>
-                            <BodyShort>
-                                På denne siden vil det dukke opp nytt innhold fortløpende. Vi setter stor pris på dine
-                                tilbakemeldinger, som du kan gi når som helst og så mange ganger du vil via skjemaet
-                                under. Takk for at du deltar!
-                            </BodyShort>
-                            <a href="https://forms.office.com/e/w3jHFRCKEC">
-                                Gi tilbakemeldinger i Forms{' '}
-                                <ExternalLinkIcon title="Ikon som illustrerer en ekstern lenke" aria-hidden={true} />
-                            </a>
-                        </Alert>
                         <Nokkelinfo />
                         <Chips size="small" className="overblikk_chips">
                             {valgteInformasjonsbokser.map((alternativ) => (
