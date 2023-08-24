cd155e527c4c68547a7118e987a6c2a8c0780232
Oda Dahlen <oda.dahlen@nav.no>
2023-08-24 10:12:40 +0200

    Høyrejusterte lenka

--- src/components/personalia/endre-personopplysninger.tsx
+++ src/components/personalia/endre-personopplysninger.tsx
@@ -1,16 +1,16 @@
-import { Link } from '@navikt/ds-react';
-import { lagPersonforvalterLenke } from '../../utils';
-import { useAktorId } from '../../data/api/fetch.ts';
-import { ExternalLinkIcon } from '@navikt/aksel-icons';
+import {BodyShort, Link} from '@navikt/ds-react';
+import {lagPersonforvalterLenke} from '../../utils';
+import {useAktorId} from '../../data/api/fetch.ts';
+import {ExternalLinkIcon} from '@navikt/aksel-icons';
 
 export function EndrePersonopplysninger(props: { fnr: string }) {
     const aktorId = useAktorId(props.fnr);
     const pdlWebUrl = aktorId.data?.aktorId ? lagPersonforvalterLenke(aktorId.data?.aktorId) : '';
 
     return (
-        <Link href={pdlWebUrl} target="_blank">
-            Endre personopplysninger
-            <ExternalLinkIcon title="Ikon som illustrerer at man åpner en ny fane" />
+        <Link href={pdlWebUrl} target="_blank" className="personopplysningslenke">
+            <BodyShort size="small"> Endre personopplysninger </BodyShort>
+            <ExternalLinkIcon title="Ikon som illustrerer at man åpner en ny fane"/>
         </Link>
     );
 }
