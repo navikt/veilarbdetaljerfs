33b196b5b63fc962dfaed52b7a2fc0ec6b91ec84
Mathias S. Müller <mathias.sundt.muller@nav.no>
2023-09-19 12:45:43 +0200

    Refaktorer epost-komponent

--- src/components/personalia/e-post.tsx
+++ src/components/personalia/e-post.tsx
@@ -4,33 +4,33 @@ import { isNullOrUndefined } from '../../utils/felles-typer';
 import { hentKilde } from '../../utils/konstanter';
 import EMDASH from '../../utils/emdash';
 
+const Epostinnhold = ({ epost }: Pick<PersonaliaInfo, 'epost'>) => {
+    if (isNullOrUndefined(epost?.epostAdresse)) {
+        return <BodyShort size="small">{EMDASH}</BodyShort>;
+    }
+
+    const { epostAdresse, epostSistOppdatert, master } = epost!;
+    return (
+        <>
+            <BodyShort size="small" className="copy_body">
+                {epostAdresse}
+                {epostAdresse && <CopyButton size="xsmall" copyText={epostAdresse} />}
+            </BodyShort>
+            <Detail className="kilde_tekst">
+                Registrert {epostSistOppdatert && epostSistOppdatert}
+                {` ${hentKilde(master)}`}
+            </Detail>
+        </>
+    );
+};
+
 function Epost({ epost }: Pick<PersonaliaInfo, 'epost'>) {
-    const Epostinnhold = () => {
-        if (isNullOrUndefined(epost?.epostAdresse)) {
-            return <BodyShort size="small">{EMDASH}</BodyShort>;
-        }
-
-        const { epostAdresse, epostSistOppdatert, master } = epost!;
-        return (
-            <>
-                <BodyShort size="small" className="copy_body">
-                    {epostAdresse}
-                    {epostAdresse && <CopyButton size="xsmall" copyText={epostAdresse} />}
-                </BodyShort>
-                <Detail className="kilde_tekst">
-                    Registrert {epostSistOppdatert && epostSistOppdatert}
-                    {` ${hentKilde(master)}`}
-                </Detail>
-            </>
-        );
-    };
-
     return (
         <div className="enkeltinfo_value">
             <BodyShort size="small" className="body_header">
                 E-post
             </BodyShort>
-            <Epostinnhold />
+            <Epostinnhold epost={epost} />
         </div>
     );
 }
