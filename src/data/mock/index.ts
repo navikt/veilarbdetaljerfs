c75ea2a64fc7a367f6455d9aec017292c8bc2cfb
Sondre Ovrid <111113539+slovrid@users.noreply.github.com>
2023-11-24 16:27:22 +0100

    TC-513 Migrer til msw major versjon 2 (#124)

--- src/data/mock/index.ts
+++ src/data/mock/index.ts
@@ -1,9 +1,5 @@
-import { setupWorker } from 'msw';
+import { setupWorker } from 'msw/browser';
 import { allHandlers } from './api';
 
 // This configures a Service Worker with the given request handlers.
-export const worker = setupWorker(...allHandlers)
-    .start({ serviceWorker: { url: `${import.meta.env.BASE_URL}mockServiceWorker.js` } })
-    .catch((e) => {
-        console.error('Unable to setup mocked API endpoints', e);
-    });
+export const worker = setupWorker(...allHandlers);
