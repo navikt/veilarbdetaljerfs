a95356474fa12ca20004ab24649cb21b9d4efd34
Sondre Ovrid <111113539+slovrid@users.noreply.github.com>
2023-11-24 16:27:22 +0100

    TC-513 Migrer til msw major versjon 2 (#124)

--- src/data/mock/api/index.ts
+++ src/data/mock/api/index.ts
@@ -4,6 +4,8 @@ import { veilarbveilederHandlers } from './veilarbveileder';
 import { veilarbfilterHandlers } from './veilarbfilter';
 import { RequestHandler } from 'msw';
 
+export const DEFAULT_DELAY_MILLISECONDS: number = 100;
+
 export const allHandlers: RequestHandler[] = [
     ...veilarbveilederHandlers,
     ...veilarbpersonHandlers,
