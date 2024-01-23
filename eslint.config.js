ed5380396e7e65c8fe4f8b44414e770355dd797e
Mathias S. Müller <mathias.sundt.muller@nav.no>
2024-01-23 11:26:38 +0100

    Fiks MSW-oppsett og eslint-konfig

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+export default [
+    {
+        env: { browser: true, es2020: true },
+        extends: [
+            'eslint:recommended',
+            'plugin:@typescript-eslint/recommended',
+            'plugin:react-hooks/recommended',
+            'plugin:jsx-a11y/recommended'
+        ],
+        parser: '@typescript-eslint/parser',
+        parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+        plugins: ['@typescript-eslint', 'react-refresh'],
+        rules: {
+            'react-refresh/only-export-components': 'warn',
+            '@typescript-eslint/ban-ts-comment': 'off'
+        },
+        root: true
+    }
+];
