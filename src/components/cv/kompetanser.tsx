3b5784192442fc536f606a5e6e6452a7a68326d1
Mathias S. Müller <mathias.sundt.muller@nav.no>
2024-01-23 11:27:07 +0100

    Importer List.Item istedenfor ListItem

--- src/components/cv/kompetanser.tsx
+++ src/components/cv/kompetanser.tsx
@@ -2,16 +2,15 @@ import { BodyShort, List } from '@navikt/ds-react';
 import { Jobbprofil } from '../../data/api/datatyper/arenaperson';
 import EMDASH from '../../utils/emdash';
 import Informasjonsbolk from '../felles/informasjonsbolk';
-import ListItem from '@navikt/ds-react/esm/list/ListItem';
 import Kompetanserikon from './ikoner/kompetanser.svg?react';
 
 const Kompetanser = ({ kompetanse }: Pick<Jobbprofil, 'kompetanse'>) => {
     const kompetanser =
         kompetanse && kompetanse.length > 0
             ? kompetanse?.map((kompetansen, index) => (
-                  <ListItem key={index}>
+                  <List.Item key={index}>
                       <BodyShort size="small">{kompetansen.tittel}</BodyShort>{' '}
-                  </ListItem>
+                  </List.Item>
               ))
             : EMDASH;
 
