a6ad58b06c84ddab3c7dd01fca977c43dec36376
Mathias S. Müller <mathias.sundt.muller@nav.no>
2024-01-24 12:39:01 +0100

    Oppdater til eslint flat config

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,35 @@
+import js from '@eslint/js';
+import tslintPlugin from '@typescript-eslint/eslint-plugin';
+import tslintParser from '@typescript-eslint/parser';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import globals from 'globals';
+
 export default [
     {
-        env: { browser: true, es2020: true },
-        extends: [
-            'eslint:recommended',
-            'plugin:@typescript-eslint/recommended',
-            'plugin:react-hooks/recommended',
-            'plugin:jsx-a11y/recommended'
-        ],
-        parser: '@typescript-eslint/parser',
-        parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-        plugins: ['@typescript-eslint', 'react-refresh'],
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                ...globals.es2024
+            },
+            parser: tslintParser,
+            parserOptions: { ecmaVersion: 'latest', sourceType: 'module' }
+        },
+        files: ['**/*.js', '**/*.ts', '**/*.tsx'],
+        plugins: {
+            '@typescript-eslint': tslintPlugin,
+            'jsx-a11y': jsxA11y,
+            'react-hooks': reactHooks,
+            'react-refresh': reactRefresh
+        },
         rules: {
+            ...js.configs.recommended.rules,
+            ...tslintPlugin.configs.recommended.rules,
+            ...jsxA11y.configs.recommended.rules,
+            ...reactHooks.configs.recommended.rules,
             'react-refresh/only-export-components': 'warn',
             '@typescript-eslint/ban-ts-comment': 'off'
-        },
-        root: true
+        }
     }
 ];
