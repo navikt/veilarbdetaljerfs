fce458b90926f6c2e29ce08d41bd1694ae64a989
Mathias S. Müller <85442404+Mathiamu@users.noreply.github.com>
2024-02-26 14:04:25 +0100

    Bump pakker, DSv6 (#158)

--- eslint.config.js
+++ eslint.config.js
@@ -1,35 +1,28 @@
-import js from '@eslint/js';
-import tslintPlugin from '@typescript-eslint/eslint-plugin';
-import tslintParser from '@typescript-eslint/parser';
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
 import jsxA11y from 'eslint-plugin-jsx-a11y';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import globals from 'globals';
 
-export default [
-    {
-        languageOptions: {
-            globals: {
-                ...globals.browser,
-                ...globals.es2024
-            },
-            parser: tslintParser,
-            parserOptions: { ecmaVersion: 'latest', sourceType: 'module' }
+export default tseslint.config(eslint.configs.recommended, ...tseslint.configs.recommended, {
+    languageOptions: {
+        globals: {
+            ...globals.browser,
+            ...globals.es2024
         },
-        files: ['**/*.js', '**/*.ts', '**/*.tsx'],
-        plugins: {
-            '@typescript-eslint': tslintPlugin,
-            'jsx-a11y': jsxA11y,
-            'react-hooks': reactHooks,
-            'react-refresh': reactRefresh
-        },
-        rules: {
-            ...js.configs.recommended.rules,
-            ...tslintPlugin.configs.recommended.rules,
-            ...jsxA11y.configs.recommended.rules,
-            ...reactHooks.configs.recommended.rules,
-            'react-refresh/only-export-components': 'warn',
-            '@typescript-eslint/ban-ts-comment': 'off'
-        }
+        parserOptions: { ecmaVersion: 'latest', sourceType: 'module' }
+    },
+    files: ['**/*.js', '**/*.ts', '**/*.tsx'],
+    plugins: {
+        'jsx-a11y': jsxA11y,
+        'react-hooks': reactHooks,
+        'react-refresh': reactRefresh
+    },
+    rules: {
+        ...jsxA11y.configs.recommended.rules,
+        ...reactHooks.configs.recommended.rules,
+        'react-refresh/only-export-components': 'warn',
+        '@typescript-eslint/ban-ts-comment': 'off'
     }
-];
+});
