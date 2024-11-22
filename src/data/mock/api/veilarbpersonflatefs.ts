f408d2d34fbf9d44f32ca3b23cbfd6acc2adf57e
Sneha Desai <sneha.desai@nav.no>
2024-11-22 10:16:31 +0100

    Fjernet feature toggle for fullmakt sin ny kilde siden den er prodsatt

--- src/data/mock/api/veilarbpersonflatefs.ts
+++ src/data/mock/api/veilarbpersonflatefs.ts
@@ -1,13 +1,8 @@
-import {
-    OboFeatureToggles,
-    BRUK_NY_KILDE_TIL_FULLMAKT,
-    VIS_INNSATSGRUPPE_HOVEDMAL_FRA_VEILARBVEDTAKSSTOTTE
-} from '../../api/fetch';
+import { OboFeatureToggles, VIS_INNSATSGRUPPE_HOVEDMAL_FRA_VEILARBVEDTAKSSTOTTE } from '../../api/fetch';
 import { delay, http, HttpResponse, RequestHandler } from 'msw';
 import { DEFAULT_DELAY_MILLISECONDS } from './index.ts';
 
 const mockFeatures: OboFeatureToggles = {
-    [BRUK_NY_KILDE_TIL_FULLMAKT]: false,
     [VIS_INNSATSGRUPPE_HOVEDMAL_FRA_VEILARBVEDTAKSSTOTTE]: false
 };
 
