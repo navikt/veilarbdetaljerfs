2e8002b98d0a4a4ab01b4411aca7690b1f34f47c
Julie Hill Roa <julie.hill.roa@nav.no>
2025-01-28 15:16:18 +0100

    Har en tilgangssjekk som dekker over komponentene i overlikk slik at de som ikke har tilgang heller ikke får se informasjon om personen (#407)

--- src/components/felles/minikomponenter.tsx
+++ src/components/felles/minikomponenter.tsx
@@ -2,13 +2,13 @@ import { Loader, Alert } from '@navikt/ds-react';
 import './minikomponenter.css';
 
 export const Laster = () => (
-    <div className="midtstill">
+    <div className="midtstill_minikomponent">
         <Loader size="3xlarge" />
     </div>
 );
 
 export const Errormelding = () => (
-    <div className="midtstill">
+    <div className="midtstill_minikomponent">
         <Alert variant="error" size="small">
             Noe gikk galt! Prøv igjen om noen minutter.
         </Alert>
