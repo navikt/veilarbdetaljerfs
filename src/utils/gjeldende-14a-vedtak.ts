4961ef6115cefb76fef3eef8486f3b7f68b2a3f8
Sondre Larsen Ovrid <sondre.larsen.ovrid@nav.no>
2025-02-18 14:03:55 +0100

    Gjer sjekken for kva vi anser som gjeldande § 14 a-vedtak meir "relaxed"

--- src/utils/gjeldende-14a-vedtak.ts
+++ src/utils/gjeldende-14a-vedtak.ts
@@ -1,5 +1,5 @@
 import { GjeldendeOppfolgingsperiode, Siste14aVedtak } from '../data/api/fetch.ts';
-import { isAfter, isBefore, parseISO } from 'date-fns';
+import { isAfter, isBefore, parseISO, subDays } from 'date-fns';
 
 export const sjekkOmSiste14aVedtakErGjeldende = (
     siste14avedtak: Siste14aVedtak | undefined,
@@ -14,7 +14,7 @@ export const sjekkOmSiste14aVedtakErGjeldende = (
     const oppfolgingsperiodeLanseringsdato = parseISO('2017-12-04T00:00:00+02:00');
 
     return (
-        isAfter(siste14aVedtakfattetDatoIso, gjeldendeOppfolgingsperiodeStartDato) ||
+        isAfter(siste14aVedtakfattetDatoIso, subDays(gjeldendeOppfolgingsperiodeStartDato, 4)) ||
         (isBefore(siste14aVedtakfattetDatoIso, oppfolgingsperiodeLanseringsdato) &&
             !isAfter(gjeldendeOppfolgingsperiodeStartDato, oppfolgingsperiodeLanseringsdato))
     );
