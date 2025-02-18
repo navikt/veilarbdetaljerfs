ce098d7e9f72b7473904c797acfe0a495554506d
Sondre Larsen Ovrid <sondre.larsen.ovrid@nav.no>
2025-02-18 14:09:37 +0100

    Legg til kommentar om "grace periode"

--- src/utils/gjeldende-14a-vedtak.ts
+++ src/utils/gjeldende-14a-vedtak.ts
@@ -13,6 +13,10 @@ export const sjekkOmSiste14aVedtakErGjeldende = (
     const gjeldendeOppfolgingsperiodeStartDato = parseISO(gjeldendeOppfolgingsperiode.startDato);
     const oppfolgingsperiodeLanseringsdato = parseISO('2017-12-04T00:00:00+02:00');
 
+    // 2025-02-18
+    // Vi har oppdaget at vedtak fattet i Arena får "fattetDato" lik midnatt den dagen vedtaket ble fattet.
+    // Derfor har vi valgt å innfør en "grace periode" på 4 døgn. Dvs. dersom vedtaket ble fattet etter
+    // "oppfølgingsperiode startdato - 4 døgn", så anser vi det som gjeldende.
     return (
         isAfter(siste14aVedtakfattetDatoIso, subDays(gjeldendeOppfolgingsperiodeStartDato, 4)) ||
         (isBefore(siste14aVedtakfattetDatoIso, oppfolgingsperiodeLanseringsdato) &&
