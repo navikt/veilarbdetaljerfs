fa36556e5838ddc69239c79f2134b1e950e427cb
Sneha Desai <sneha.desai@nav.no>
2025-11-12 16:11:16 +0100

    Fikset feil oppdaget av prettier:check

--- src/amplitude/taxonomy-events.ts
+++ src/amplitude/taxonomy-events.ts
@@ -1,3 +1,5 @@
 //Før du lager en ny eventType -> sjekk https://github.com/navikt/analytics-taxonomy/tree/main/events
-export type AmplitudeEvent =
-    | { name: 'knapp klikket'; data: { knapptekst: string } };
+export type AmplitudeEvent = {
+    name: 'knapp klikket';
+    data: { knapptekst: string };
+};
