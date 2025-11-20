52c5db09d4be1fb0ebcfca2dbc81237c94e18b35
Kristina Skatun <kristinaskatun@gmail.com>
2025-11-20 14:07:37 +0100

    Oppdater lenketekst og legg til i nøkkelinformasjon

--- src/components/ytelser-innhold.tsx
+++ src/components/ytelser-innhold.tsx
@@ -1,31 +1,23 @@
-import { Alert, Link } from '@navikt/ds-react';
+import { Alert } from '@navikt/ds-react';
 import { useAppStore } from '../stores/app-store';
 import { Errormelding, Laster } from './felles/minikomponenter';
 import EMDASH from '../utils/emdash';
 import { EnkeltInformasjon } from './felles/enkeltInfo';
-import { isNotEmptyArray } from '../utils/felles-typer';
 import { useYtelser } from '../data/api/fetch';
 import Informasjonsbolk from './felles/informasjonsbolk';
 import { formaterDato } from '../utils/formater.ts';
-import { lagModiaPersonoversiktLenke } from '../utils';
+import { isNotEmptyArray } from '../utils/felles-typer.ts';
+import { ModiaPersonoversiktLenke } from './modia-personoversikt-lenke.tsx';
 
 const Ytelsesinnhold = () => {
     const { fnr } = useAppStore();
-
     const { data: ytelserData, error: ytelserError, isLoading: ytelserLoading } = useYtelser(fnr);
-    const modiaPersonoversiktLenke = lagModiaPersonoversiktLenke();
 
     if (ytelserLoading) {
         return <Laster />;
     }
 
-    if (!ytelserData || !isNotEmptyArray(ytelserData?.vedtak)) {
-        return (
-            <Alert inline variant="info" size="small">
-                Ingen Arena-ytelser registrert.
-            </Alert>
-        );
-    }
+    const harIngenArenaYtelser = !ytelserData || !isNotEmptyArray(ytelserData?.vedtak);
 
     if (ytelserError) {
         return <Errormelding />;
@@ -33,28 +25,31 @@ const Ytelsesinnhold = () => {
 
     return (
         <>
+            <Alert variant="info" size="small" className="alert_container">
+                Viser kun ytelsesvedtak gjort i Arena (AAP, tiltakspenger, og dagpenger). <ModiaPersonoversiktLenke />
+            </Alert>
             <span className="info_container">
-                <Alert variant="info" size="small">
-                    Viser kun ytelsesvedtak gjort i Arena (AAP, tiltakspenger, og dagpenger).{' '}
-                    <Link href={modiaPersonoversiktLenke} target="_blank" rel="noopener">
-                        Sjekk modia personoversikt.
-                    </Link>
-                </Alert>
-                {ytelserData?.vedtak.map((vedtaket, index) => (
-                    <div key={index}>
-                        <Informasjonsbolk header={vedtaket.type || EMDASH} headerTypo="ingress">
-                            <EnkeltInformasjon header="Vedtakstatus" value={vedtaket.status || EMDASH} />
-                            <EnkeltInformasjon header="Aktivitetsfase" value={vedtaket.aktivitetsfase || EMDASH} />
-                            <EnkeltInformasjon
-                                header="Fra dato / Til dato"
-                                value={`                       
+                {harIngenArenaYtelser ? (
+                    <Alert inline variant="info" size="small">
+                        Ingen Arena-ytelser registrert.
+                    </Alert>
+                ) : (
+                    ytelserData?.vedtak.map((vedtaket, index) => (
+                        <div key={index}>
+                            <Informasjonsbolk header={vedtaket.type || EMDASH} headerTypo="ingress">
+                                <EnkeltInformasjon header="Vedtakstatus" value={vedtaket.status || EMDASH} />
+                                <EnkeltInformasjon header="Aktivitetsfase" value={vedtaket.aktivitetsfase || EMDASH} />
+                                <EnkeltInformasjon
+                                    header="Fra dato / Til dato"
+                                    value={`                       
                                 ${vedtaket.fraDato ? `${formaterDato(vedtaket.fraDato)}` : 'Ingen fra-dato'} – ${
                                     vedtaket.tilDato ? `${formaterDato(vedtaket.tilDato)}` : 'Ingen til-dato'
                                 }`}
-                            />
-                        </Informasjonsbolk>
-                    </div>
-                ))}
+                                />
+                            </Informasjonsbolk>
+                        </div>
+                    ))
+                )}
             </span>
         </>
     );
