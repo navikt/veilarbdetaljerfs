d6db137508d1d492f680c1260ecc8c0a7fbecf92
Mathias S. Müller <mathias.sundt.muller@nav.no>
2026-02-09 15:05:11 +0100

    Bump og fiks utdatert konfig

--- eslint.config.js
+++ eslint.config.js
@@ -2,27 +2,31 @@ import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import jsxA11y from 'eslint-plugin-jsx-a11y';
 import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
 import globals from 'globals';
+import { defineConfig } from 'eslint/config';
+import { reactRefresh } from 'eslint-plugin-react-refresh';
 
-export default tseslint.config(eslint.configs.recommended, ...tseslint.configs.recommended, {
-    languageOptions: {
-        globals: {
-            ...globals.browser,
-            ...globals.es2024
+export default defineConfig([
+    eslint.configs.recommended,
+    tseslint.configs.recommended,
+    {
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                ...globals.es2024
+            },
+            parserOptions: { ecmaVersion: 'latest', sourceType: 'module' }
         },
-        parserOptions: { ecmaVersion: 'latest', sourceType: 'module' }
+        files: ['**/*.js', '**/*.ts', '**/*.tsx'],
+        plugins: {
+            'jsx-a11y': jsxA11y,
+            'react-hooks': reactHooks
+        },
+        rules: {
+            ...jsxA11y.configs.recommended.rules,
+            ...reactHooks.configs.recommended.rules,
+            '@typescript-eslint/ban-ts-comment': 'off'
+        }
     },
-    files: ['**/*.js', '**/*.ts', '**/*.tsx'],
-    plugins: {
-        'jsx-a11y': jsxA11y,
-        'react-hooks': reactHooks,
-        'react-refresh': reactRefresh
-    },
-    rules: {
-        ...jsxA11y.configs.recommended.rules,
-        ...reactHooks.configs.recommended.rules,
-        'react-refresh/only-export-components': 'warn',
-        '@typescript-eslint/ban-ts-comment': 'off'
-    }
-});
+    reactRefresh.configs.vite()
+]);
