aa2d09a775fa61b607cf010df00439e7402325d5
Klara Margrethe Helgemo <klara.margrethe.helgemo@nav.no>
2026-02-12 21:05:53 +0100

    Setter opp logikk for visning av navn og alder på barn. Se veilarbperson PersonV2DataMapper for forklaring av hva som  vises for ulike kombinasjoner.

--- src/utils/barn-utils.ts
+++ src/utils/barn-utils.ts
@@ -1,11 +1,5 @@
 import { Gradering, PersonsBarn } from '../data/api/datatyper/personalia.ts';
 
-/** Finner ut hvor gammel en person er i dag */
-export function kalkulerAlder(fodselsdato: Date): number {
-    const diff = Date.now() - fodselsdato.getTime();
-    return new Date(diff).getUTCFullYear() - 1970;
-}
-
 /** Returnerer alder på barn, eller 'DØD' dersom barnet ikke lever lenger. */
 export function finnAlderTekstForBarn(personalia: { alder: number; erDod: boolean }): string {
     if (personalia.erDod) {
@@ -42,5 +36,5 @@ export const finnNavnOgAlderTekstForBarn = (barn: PersonsBarn) => {
 
 /** Returnerer alle barn som er under en viss alder. */
 export const finnBarnUnderEnBestemtAlder = (alleBarn: PersonsBarn[], maksalder: number) => {
-    return alleBarn.filter((barn) => kalkulerAlder(new Date(barn.fodselsdato)) < maksalder);
+    return alleBarn.filter((barn) => barn.alder < maksalder);
 };
