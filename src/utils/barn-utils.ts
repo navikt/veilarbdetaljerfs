f45fbc114de70a6586a2eda00fa0da1737095985
Klara Margrethe Helgemo <klara.margrethe.helgemo@nav.no>
2026-02-12 21:05:53 +0100

    Setter opp logikk for visning av navn og alder på barn. Se veilarbperson PersonV2DataMapper for forklaring av hva som  vises for ulike kombinasjoner.

--- src/utils/barn-utils.ts
+++ src/utils/barn-utils.ts
@@ -20,10 +20,18 @@ export function finnAlderTekstForBarn(personalia: { alder: number; erDod: boolea
  * Dersom barnet har adressebeskyttelse maskeres denne informasjonen.
  * */
 export const finnNavnOgAlderTekstForBarn = (barn: PersonsBarn) => {
-    if (barn.gradering === Gradering.UGRADERT || barn.erDod || barn.harVeilederTilgang) {
+
+    const adressebeskyttet = barn.gradering.includes(Gradering.STRENGT_FORTROLIG) || barn.gradering.includes(Gradering.FORTROLIG) || barn.gradering.includes(Gradering.STRENGT_FORTROLIG_UTLAND);
+    // fornavn DØD
+    if (barn.erDod) { return `${barn.fornavn} (${finnAlderTekstForBarn(barn)})`}
+    //'Barn (skjermet)' alder
+    if (!adressebeskyttet && !barn.harVeilederTilgang && barn.erEgenAnsatt) {
+        return `'Barn (skjermet)' (${finnAlderTekstForBarn(barn)})`;}
+    //'Barn (adressebeskyttelse)'
+    if (adressebeskyttet && !barn.harVeilederTilgang ) {
+        return 'Barn (adressebeskyttelse)';}
+    //Når veileder har tilgang
         return `${barn.fornavn} (${finnAlderTekstForBarn(barn)})`;
-    }
-    return 'Barn (adressebeskyttelse)';
 };
 
 /** Returnerer alle barn som er under en viss alder. */
